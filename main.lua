7fc885ecb6ccd4aeea3fb63a6251268a3cffde34
Amokrane <amokrane123123@gmail.com>
2021-12-09 22:30:12 +0100

    modified to match the new files

--- main.lua
+++ main.lua
@@ -1,44 +1,54 @@
-isDebug = true
 love.graphics.setDefaultFilter("nearest", "nearest")
-local Map = require'map'
-local Player = require'player'
-local timer = require'libs/timer'
-local Pigs = require'Ennemis/Pigs'
-function love.load()
-  Effect = true
+wf = require'libs/windfield'
+sti = require'libs/sti'
+camera = require'libs/camera'
+anim8 = require "libs/anim8"
 
-  wf = require'libs/windfield'
-  sti = require'libs/sti'
-  camera = require'libs/camera'
-  anim8 = require "libs/anim8"
-  require'camera'
+
+local Map = require('map')
+local Player = require('player')
+local timer = require('libs/timer')
+local Pigs = require('Ennemis/Pigs')
+local cam = require('camera')
+
+function love.load()
+
+  isDebug = true
   Map:load()
   Player:load()
+  Pigs.LoadAssets(Pigs)
+  cam:Load()
 end
 
 function love.update(dt)
-
   Player:update(dt)
   Pigs.updateAll(dt)
   cam:update(dt)
   Map:update(dt)
   world:update(dt)
-
 end
 
 function love.draw()
-      cam:attach()
-    Map:draw()
-    Pigs.drawAll()
-    Player:draw()
-    if isDebug then world:draw() end
-      cam:detach()
+  cam:attach()
+  Map:draw()
+  Pigs.drawAll()
+  Player:draw()
+  if isDebug then world:draw() end
+  cam:detach()
 
-    if isDebug then
-      love.graphics.print(Player.xVel)
-      if Player.grounded then love.graphics.print('On Ground' , 0 , 10)
-      else love.graphics.print('Flying', 0 , 10) end
-    end
+  if isDebug then
+  love.graphics.print(Player.xVel)
+    if Player.grounded then
+      love.graphics.print('On Ground' , 0 , 10)
+    else love.graphics.print('Flying', 0 , 10) end
+  end
+end
+function love.keypressed(key)
+  Player:Jump(key)
+  debug(key)
+  log(key)
+  Exit (key)
+  cam:LockToPlayer(key)
 end
 
 function debug(key)
@@ -49,23 +59,29 @@ function debug(key)
   end
 end
 
-function log(key)
-  if key == 'ctrl' and isDebug then
-    print(Player.x)
+function Exit(key)
+  if key == 'escape' then
+    love.event.quit()
   end
 end
 
-function love.keypressed(key)
-  Player:Jump(key)
-  debug(key)
-  log(key)
+function log(key,content)
+  if key == '0' and isDebug then
+  end
 end
+
 function love.keyreleased(key)
   Player:Friction(key)
 end
 
-function love.mousepressed(x, y, button)
+function love.mousepressed(x, y,key)
+  Player:FastAttack(key)
 end
+
 function love.mousereleased(x, y, button)
   Player:AttackTimer(button)
+end
+
+function love.wheelmoved(x, y)
+  cam:Zoom(x,y)
 end
