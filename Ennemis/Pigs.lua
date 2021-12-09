41fa02c119bae0d9fb628932aa20216a754a2c49
Amokrane <amokrane123123@gmail.com>
2021-12-09 22:28:52 +0100

    added one ennemi , animations , basic IA

--- Ennemis/Pigs.lua
+++ Ennemis/Pigs.lua
@@ -1,43 +1,49 @@
 local Pigs = {img = love.graphics.newImage('assets/pig.png')}
 Pigs.__index = Pigs
-
 local ActivePigs = {}
-function Pigs.new(x,y,width,height)
-  local instance = setmetatable({}, Pigs)
-  instance.x = x
-  instance.y = y
-  instance.width = width
-  instance.height = height
-  instance.stat = 'idle'
-  instance.direction = 'left'
-  instance.collider = world:newRectangleCollider(x,y,width ,height)
-  instance.collider:setType('dynamic')
-  instance.collider:setFixedRotation(true)
-  instance.collider:setCollisionClass('Ennemy')
-  instance.sheet = love.graphics.newImage('assets/pig.png')
-  instance.grid = anim8.newGrid( 34, 28, instance.sheet:getWidth(), instance.sheet:getHeight())
-  instance.animation = {}
-  instance.animation.walk = anim8.newAnimation( instance.grid('1-6' , 1), 0.1)
-  instance.animation.idle = anim8.newAnimation( instance.grid('1-1' , 1), 0.1)
-  table.insert(ActivePigs, instance)
 
+function Pigs.new(x,y,width,height,speed)
+    local instance = setmetatable({}, Pigs)
+    instance.x = x
+    instance.y = y
+    instance.width = width
+    instance.height = height
+    instance.speed = speed
+    instance.stat = 'idle'
+    instance.direction = 1
+    instance.speed = speed
+    instance.collider = world:newRectangleCollider(x,y,width ,height)
+    instance.collider:setType('dynamic')
+    instance.collider:setFixedRotation(true)
+    instance.collider:setCollisionClass('Ennemy')
+    table.insert(ActivePigs, instance)
+end
+
+function Pigs.LoadAssets(self)
+  self.sheet = love.graphics.newImage('assets/pig.png')
+  self.grid = anim8.newGrid( 34, 28, self.sheet:getWidth(), self.sheet:getHeight())
+  self.setAnimations(Pigs)
+end
+
+function Pigs.setAnimations(self)
+  self.animation = {}
+  self.animation.walk = anim8.newAnimation( self.grid('1-6' , 1), 0.1)
+  self.animation.idle = anim8.newAnimation( self.grid('1-1' , 1), 0.1)
 end
 
 function Pigs:update(dt)
   self.xVel , self.yVel = self.collider:getLinearVelocity()
+  self:AI(dt)
   self:setDirection(dt)
   self:setStat(dt)
   self:Animate(dt)
   self:sync(dt)
-end
-
-function Pigs:animations(args)
-
+  self:ForceMove(dt)
 end
 
 function Pigs:Animate(dt)
-  self.anim = self.animation[self.stat]
-  self.anim:update(dt)
+  self.animation.current = self.animation[self.stat]
+  self.animation.current:update(dt)
 end
 
 function Pigs:sync(dt)
@@ -48,20 +54,35 @@ function Pigs:setStat(dt)
   if self.xVel ~= 0 then self.stat = 'walk'
   else self.stat = 'idle' end
 end
+
 function Pigs:setDirection(dt)
-  if self.xVel < 0 then self.direction = 'left'
-  elseif self.xVel > 0 then self.direction = 'right' end
+  if self.xVel > 0 then self.direction = 1 elseif self.xVel < 0 then self.direction = -1 end
+end
+
+function Pigs:AI(dt)
+  if self:checkNoGround(dt) or self:checkWalls(dt) then
+    self.direction = -self.direction
+  end
+  self.collider:setLinearVelocity(self.direction*self.speed , self.yVel)
+end
+
+function Pigs:checkNoGround(dt)
+    local query = world:queryRectangleArea(self.x+self.direction*self.width/2+self.direction,self.y+self.height/2 , 1 , 1 , {'Ground','Platforms'})
+    if #query == 0 then
+    return true end
+
+end
+
+function Pigs:checkWalls(dt)
+  local query = world:queryRectangleArea(self.x-self.width/2-2,self.y-self.height/2 , 2 , self.height , {'Walls'})
+  local query = world:queryRectangleArea(self.x+self.width/2,self.y-self.height/2 , 2 , self.height , {'Walls'})
+  if #query > 0 then
+  return true end
 end
 
 function Pigs:draw()
-  local scale = 1
-  if self.direction == 'left' then scale = 1 else scale = -1 end
-  self.anim:draw(self.sheet,self.x,self.y,0 , scale , 1 , self.width , self.height+3)
+  self.animation.current:draw(self.sheet,self.x,self.y,0 , -self.direction , 1 , self.width , self.height+3)
 end
-
-
-
-
 
 function Pigs.updateAll(dt)
    for i,instance in ipairs(ActivePigs) do
@@ -73,6 +94,17 @@ function Pigs.drawAll()
    for i,instance in ipairs(ActivePigs) do
       instance:draw()
    end
+end
+
+function Pigs:ForceMove(dt)
+  if isDebug then
+    if love.keyboard.isDown('j') then
+      self.collider:setLinearVelocity(-100 , self.yVel)
+    end
+    if love.keyboard.isDown('l') then
+      self.collider:setLinearVelocity(100 , self.yVel)
+    end
+  end
 end
 
 return Pigs
