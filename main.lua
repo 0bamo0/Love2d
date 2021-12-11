7ff92bf537e81f4cf328b20cb171d30fce86da9c
Amokrane <amokrane123123@gmail.com>
2021-12-12 00:49:01 +0100

    Minor edits

--- main.lua
+++ main.lua
@@ -1,63 +1,67 @@
 love.graphics.setDefaultFilter("nearest", "nearest")
-wf = require'libs/windfield'
-sti = require'libs/sti'
-camera = require'libs/camera'
+wf = require "libs/windfield"
+sti = require "libs/sti"
+camera = require "libs/camera"
 anim8 = require "libs/anim8"
 
-
-local Map = require('map')
-local Player = require('player')
-local timer = require('libs/timer')
-local Pigs = require('Ennemis/Pigs')
-local cam = require('camera')
-local debugging = require('debugging')
+local Map = require("map")
+local Player = require("player")
+local timer = require("libs/timer")
+local Pigs = require("Ennemis/Pigs")
+local cam = require("camera")
+local debugging = require("debugging")
 
 function love.load()
-  Map:load()
-  Player:load()
-  cam:Load()
-
-  debugging:load()
+    Map:load()
+    Player:load()
+    cam:Load()
+    debugging:load()
 end
 
 function love.update(dt)
-  Player:update(dt)
-  Pigs.updateAll(dt)
-  cam:update(dt)
-  world:update(dt)
+    Pigs.updateAll(dt)
+    Player:update(dt)
+    cam:update(dt)
+    world:update(dt)
+    debugging:update(dt)
+    Map:update(dt)
 end
 
 function love.draw()
-  cam:attach()
-  Map:draw()
-  Pigs.drawAll()
-  Player:draw()
-  if debugging.isActif then world:draw() end
-  cam:detach()
-  debugging:draw()
+    cam:attach()
+    Map:draw()
+    Pigs.drawAll()
+    if debugging.isActif then
+        world:draw()
+    end
+    Player:draw()
+    cam:detach()
+    debugging:draw()
 end
+
 function love.keypressed(key)
-  Player:Jump(key)
-  debugging:Switch(key)
-  Exit (key)
-  cam:LockToPlayer(key)
+    Player:Jump(key)
+    debugging:Switch(key)
+    Exit(key)
+    cam:LockToPlayer(key)
 end
 
 function love.keyreleased(key)
-  Player:Friction(key)
+    Player:Friction(key)
 end
 
-function love.mousepressed(x, y,key)
+function love.mousepressed(x, y, key)
 end
 
 function love.mousereleased(x, y, button)
 end
 
 function love.wheelmoved(x, y)
+  cam:Zoom(x, y)
 end
 
 function Exit(key)
-  if key == 'escape' then
-    love.event.quit()
-  end
+    if key == "escape" then
+        love.event.quit()
+    end
 end
--- map.lua
+++ map.lua
@@ -1,58 +1,103 @@
 local Map = {}
-local Player = require'player'
-local Pigs = require'Ennemis/Pigs'
+local Player = require "player"
+local Pigs = require "Ennemis/Pigs"
+
 function Map:load()
-  world = wf.newWorld(0 , 2000 , true)
-  world:setQueryDebugDrawing(true)
-  self.currentLevel = 1
-  world:addCollisionClass('Ground')
-  world:addCollisionClass('Platforms')
-  world:addCollisionClass('Ennemy')
-  world:addCollisionClass('Walls')
-  world:addCollisionClass('Player' , {ignores = {'Ennemy'}})
-  self:init()
+    world = wf.newWorld(0, 2000, false)
+    world:setQueryDebugDrawing(true)
+    self.currentLevel = 1
+    world:addCollisionClass("Ground")
+    world:addCollisionClass("Platforms")
+    world:addCollisionClass("Ennemy")
+    world:addCollisionClass("Walls")
+    world:addCollisionClass("Player", {ignores = {"Ennemy"}})
+    world:addCollisionClass("Next", {ignores = {"Ennemy", "Player"}})
+    self:init()
 end
 
 function Map:init()
-  self.level = sti('maps/'..self.currentLevel..'.lua')
-  self.groundLayer = self.level.layers.ground
-  self.platformsLayer = self.level.layers.Platforms
-  self.entityLayer = self.level.layers.entity
-  self.wallsLayer = self.level.layers.Walls
-  for i,v in ipairs(self.groundLayer.objects) do
-    local collider = world:newRectangleCollider(v.x , v.y , v.width , v.height)
-    collider:setCollisionClass('Ground')
-    collider:setType('static')
-  end
-  for i,v in ipairs(self.platformsLayer.objects) do
-    local collider = world:newRectangleCollider(v.x , v.y , v.width , v.height)
-    collider:setCollisionClass('Platforms')
-    collider:setType('static')
-    platy = v.height
-  end
-  for i,v in ipairs(self.wallsLayer.objects) do
-    local collider = world:newRectangleCollider(v.x , v.y , v.width , v.height)
-    collider:setCollisionClass('Walls')
-    collider:setType('static')
-  end
-  self:spawnEntities()
+    self.level = sti("maps/" .. self.currentLevel .. ".lua")
+    self.level.colliders = {}
+    self.groundLayer = self.level.layers.Ground
+    self.platformsLayer = self.level.layers.Platforms
+    self.entityLayer = self.level.layers.Entity
+    self.wallsLayer = self.level.layers.Walls
+    self.nextLayer = self.level.layers.Next
+
+    for i, obj in ipairs(self.groundLayer.objects) do
+        local collider = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+        collider:setCollisionClass("Ground")
+        collider:setType("static")
+        table.insert(self.level.colliders, collider)
+    end
+    for i, obj in ipairs(self.platformsLayer.objects) do
+        local collider = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+        collider:setCollisionClass("Platforms")
+        collider:setType("static")
+        table.insert(self.level.colliders, collider)
+        platy = obj.height
+    end
+    for i, obj in ipairs(self.nextLayer.objects) do
+        local collider = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+        collider:setCollisionClass("Next")
+        collider:setType("static")
+        table.insert(self.level.colliders, collider)
+    end
+    for i, obj in ipairs(self.wallsLayer.objects) do
+        local collider = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+        collider:setCollisionClass("Walls")
+        collider:setType("static")
+        table.insert(self.level.colliders, collider)
+    end
+    for i, obj in ipairs(self.nextLayer.objects) do
+        local collider = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+        collider:setCollisionClass("Next")
+        collider:setType("static")
+        table.insert(self.level.colliders, collider)
+    end
+    self:spawnEntities()
+end
+
+function Map:update(dt)
+    if Player.collider:enter("Next") then
+        self:next()
+    end
+end
+
+function Map:next()
+    self:clean()
+    local n = love.filesystem.getDirectoryItems("maps")
+    if self.currentLevel == #n - 1 then
+        self.currentLevel = self.currentLevel
+    else
+        self.currentLevel = self.currentLevel
+    end
+    self:init()
+    Player:resetPosition()
+end
+
+function Map:clean()
+    for i, layers in ipairs(self) do
+        layers = {}
+    end
+    for i, collider in ipairs(self.level.colliders) do
+        collider:destroy()
+    end
+    Pigs.removeAll()
 end
 
 function Map:spawnEntities()
-	for i,v in ipairs(self.entityLayer.objects) do
-		if v.type == "pig" then
-			Pigs.new(v.x, v.y ,v.width , v.height,v.properties.speed )
-		end
-    if v.type == "pigsSpawnArea" then
-      for i =1 , v.properties.spawnNumber do
-			     Pigs.newArea(math.random(v.x , v.x+v.width),math.random(v.y , v.y+v.height),v.width , v.height,v.properties.speed )
-      end
-		end
-	end
+    for i, entity in ipairs(self.entityLayer.objects) do
+        if entity.type == "pig" then
+            for i = 1, entity.properties.spawnNumber do
+                Pigs.new(entity.x, entity.y, entity.width, entity.height, entity.properties.speed)
+            end
+        end
+    end
 end
 
 function Map:draw()
-  self.level:drawLayer(self.level.layers['background'])
-  self.level:drawLayer(self.level.layers['arbres'])
+    self.level:drawLayer(self.level.layers["Ground"])
 end
+
 return Map
