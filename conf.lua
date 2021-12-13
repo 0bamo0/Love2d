b85917826fa875098d872af5a5b3199082bfb7cd
Amokrane <amokrane123123@gmail.com>
2021-12-13 02:51:19 +0100

    1

--- conf.lua
+++ conf.lua
@@ -1,18 +1,10 @@
 function love.conf(t)
     t.title = "Game name"
     t.window.icon = nil
-    t.window.msaa = 1
+    t.window.msaa = 0
     t.gammacorrect = true
     t.console = false
-<<<<<<< HEAD
-<<<<<<< HEAD
     t.window.vsync = false
-=======
-    t.window.vsync = true
->>>>>>> 168b82845e327ac07397d27418ad1bb839e9812e
-=======
-    t.window.vsync = true
->>>>>>> 168b82845e327ac07397d27418ad1bb839e9812e
     t.window.fullscreen = true
     t.window.borderless = false
     t.window.highdpi = false
